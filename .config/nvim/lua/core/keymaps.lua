06a0cac3053711825dd09398b8076dff81cad0e0
Aditya Singh <adityasingh14102001@gmail.com>
2025-05-24 11:23:09 +0530

    fixes and optimizations

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,5 +9,17 @@ vim.keymap.set("n", "}", "}zz", opts)
 vim.keymap.set("n", "{", "{zz", opts)
 
 vim.keymap.set("n", "<leader>y", [["+y]], { desc = "yank to system clipboard" })
+vim.keymap.set("n", "<leader>sa", "ggVG", { desc = "Select All" })
 
 vim.keymap.set("n", "dd", [["_dd]])
+vim.keymap.set("v", "d", [["_d]])
+
+local objects = { "w", '"', "'", "`", ")", "]", "}", ">", "b", "B", "}", "]", ")", ">", '"' }
+for _, obj in ipairs(objects) do
+	vim.keymap.set("n", "ci" .. obj, '"_ci' .. obj, { noremap = true, silent = true })
+end
+
+local inside_objects = { "w", '"', "'", "`", ")", "]", "}", ">", "b", "B" }
+for _, obj in ipairs(inside_objects) do
+	vim.keymap.set("n", "di" .. obj, '"_di' .. obj, { noremap = true, silent = true })
+end
